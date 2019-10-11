bbf52a0d373e3ddbbcb0c9364564318c76a53563
konglingjiao <konglingjiao@pansoft.com>
2019-10-11 17:39:41 +0800

    修改产品与服务样式

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -42,7 +42,33 @@
                         </Dropdown>
                     </li>
                     <router-link to="/produceAndService">
-                        <li v-bind:class="{ nav_active: 3 == menuindex }"  @click="changeValue(3)" >{{$t("header.service")}}</li>
+                        <Dropdown style="z-index: 9999;position: relative" class="other">
+                            <a v-bind:class="{ nav_active1 : 31 == menuindex || 32 == menuindex || 33 == menuindex || 34 == menuindex || 35 == menuindex || 36 == menuindex || 37 == menuindex, nav_active2: 31 != menuindex && 32 != menuindex && 33 != menuindex && 34 != menuindex && 35 != menuindex && 36 != menuindex && 37 != menuindex}" href="javascript:void(0)">{{$t("header.service")}}&nbsp;<img class="icon-arrow" src="../assets/arrow_down.png"></a>
+                            <DropdownMenu class="menu_list02" slot="list">
+                                <DropdownItem>
+                                    <a v-bind:class="{nav_active1 : 31==menuindex, nav_active2: 31!=menuindex}" @click="changeValue(31)" >teleComm</a>
+                                </DropdownItem>
+                                <DropdownItem>
+                                    <a v-bind:class="{nav_active1 : 32==menuindex, nav_active2: 32!=menuindex}" @click="changeValue(32)">RPA</a>
+                                </DropdownItem>
+                                <DropdownItem>
+                                    <a v-bind:class="{nav_active1 : 33==menuindex, nav_active2: 33!=menuindex}" @click="changeValue(33)">iOSP</a>
+                                </DropdownItem>
+                                <DropdownItem>
+                                    <a v-bind:class="{nav_active1 : 34==menuindex, nav_active2: 34!=menuindex}" @click="changeValue(34)">交易所</a>
+                                </DropdownItem>
+                                <DropdownItem>
+                                    <a v-bind:class="{nav_active1 : 35==menuindex, nav_active2: 35!=menuindex}" @click="changeValue(35)">mulechain</a>
+                                </DropdownItem>
+                                <DropdownItem>
+                                    <a v-bind:class="{nav_active1 : 36==menuindex, nav_active2: 36!=menuindex}" @click="changeValue(36)">企业数据浏览器</a>
+                                </DropdownItem>
+                                <DropdownItem>
+                                    <a v-bind:class="{nav_active1 : 37==menuindex, nav_active2: 37!=menuindex}" @click="changeValue(37)">影视拍摄</a>
+                                </DropdownItem>
+                            </DropdownMenu>
+                        </Dropdown>
+<!--                        <li v-bind:class="{ nav_active: 3 == menuindex }"  @click="changeValue(3)" >{{$t("header.service")}}</li>-->
                     </router-link>
                     <router-link to="/news">
                         <li v-bind:class="{ nav_active: 4 == menuindex }"  @click="changeValue(4)" >{{$t("header.news")}}</li>
